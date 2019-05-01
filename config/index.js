5fbdcd3ee7e2a830b5e9a254309ff0aa5d8061b1
Jhonatan Souza <jhonatan.araujo1@hotmail.com>
2019-04-30 23:16:49 -0300

    fix absolute path at build deploy2

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue-push-dir',
+    assetsPublicPath: 'https://jhonatanaraujo1.github.io/Tratativa.ummense/',
 
     /**
      * Source Maps
